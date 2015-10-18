a73cc63c89ee94d7dfaef7987ef3b5bbd296beaa
jd <jd.foxsports@gmail.com>
2015-10-18 10:11:56 +0000

    modifying root routing

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-var express = require("express"),
-    bodyParser = require("body-parser"),
+var fs = require("fs"),
     path = require('path');
+    
+var express = require("express"),
+    bodyParser = require("body-parser");
+    
 var app = express();
+
 app.use(bodyParser.json());
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-app.listen(process.env.PORT,process.env.IP,function(){
-    console.log("IP:",process.env.IP);
-    console.log("port:",process.env.PORT);
-});
+app.use(express.static(__dirname + "/public"));
+
+//set root
+app.get("/",function(req,res){
+        if(req.url == "/"){
+            fs.readFile("./public/index.html", function(error, data){
+                res.writeHead(200, {"Content-type":"text/html"});
+                res.end(data);
+            });
+        } else {
+            fs.readFile("./public" + req.url, function(error, data){
+                if (error) {
+                    res.writeHead(404, {"Content-type":"text/plain"});
+                    res.end("Sorry the page was not found: ");
+                } else {
+                    res.writeHead(200, {"Content-type":"text/html"});
+                    res.end(data);
+                }
+            });
+            
+        }
+}).listen(process.env.PORT, process.env.IP);
